0323c60a90405a8c1d533fc0630d73829b89859a
Карабаев Нуриддин <94254058+asuramesocrum@users.noreply.github.com>
2022-05-26 20:24:25 +0400

    upd

---
+++ README.md
@@ -0,0 +1,15 @@
+# 3D GRAPHIC
+
+[//]: # (This project was bootstrapped with [Create React App]&#40;https://github.com/facebook/create-react-app&#41;.)
+
+### `yarn start` or `npm start`
+
+Запустит приложеине в дев режиме.\
+Перейдите по [http://localhost:3000](http://localhost:3000) что-бы посмотреть в браузере.
+
+
+### `yarn build` or `npm run build`
+
+Создаст папку в проде, с именим `build`.\
+Правильно свяжет React в рабочем режиме,\
+оптимизируя сборку ддля наилучшей производительности.
---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import "./main.scss"
+import MainComponent from "./components/mainComponent";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path={"/"} element={<MainComponent />}>
+          <Route path={"template/"} element={<MainComponent />} />
+          <Route path={"template/:name"} element={<MainComponent />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
